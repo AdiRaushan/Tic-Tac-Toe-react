4a3a3ce613b351836a2e868508c68c35a72ac402
AdiRaushan <adiraushan2k@gmail.com>
2024-04-13 09:02:12 +0530

    passing props to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
+import { useState } from "react";
 import Square from "./Square";
 
 function App() {
+  const [square, Setsquare] = useState(Array(9).fill(null));
   return (
     <>
       <h1 className="text-center">Tic Tac Toe</h1>
       <div className="mt-10">
         <div className="board-row flex justify-center">
-          <Square value={1} />
-          <Square value={2} />
-          <Square value={3} />
+          <Square value={square[0]} />
+          <Square value={square[1]} />
+          <Square value={square[2]} />
         </div>
         <div className="board-row flex justify-center">
-          <Square value={4} />
-          <Square value={5} />
-          <Square value={6} />
+          <Square value={square[3]} />
+          <Square value={square[4]} />
+          <Square value={square[5]} />
         </div>
         <div className="board-row flex justify-center">
-          <Square value={7} />
-          <Square value={8} />
-          <Square value={9} />
+          <Square value={square[6]} />
+          <Square value={square[7]} />
+          <Square value={square[8]} />
         </div>
       </div>
     </>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
-
-const Square = () => {
-  const [value, setValue] = useState(null);
-
-  return (
-    <button className="square" onClick={() => setValue("B")}>
-      {value}
-    </button>
-  );
+const Square = ({ value }) => {
+  return <button className="square">{value}</button>;
 };
 
 export default Square;
